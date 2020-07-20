8347fdb2a301f02f24c8381aa182705ce7c1a6ea
丸山　康司 <maruyama_k@popular-soft.com>
2020-07-20 19:32:33 +0900

    commit
    commit

---
+++ Python/Django/pyworks/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\maruyama-k\documents\github\study2\python\django\pyworks\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
