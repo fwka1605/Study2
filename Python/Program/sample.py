f180863bfb3c2c865e34d056ef3d09f8e8023433
丸山　康司 <maruyama_k@popular-soft.com>
2020-10-05 19:08:54 +0900

    Django book

--- Python/Program/sample.py
+++ Python/Program/sample.py
@@ -1,24 +1,7 @@
-import numpy as np
+def func3(x,y,z):
+    return x*y+z
 
-import matplotlib.pyplot as plt 
+func4 = lambda x,y,z:x*y+z
 
-from matplotlib import cm
-
-from mpl_toolkits.mplot3d import Axes3D
-
-t = np.linspace(-2*np.pi,2*np.pi)
-
-X,Y = np.meshgrid(t,t)
-
-R = np.sqrt(X**2+Y**2)
-
-Z = np.sin(R)
-
-fig = plt.figure(figsize=(6,6))
-
-ax = fig.add_subplot(1,1,1,projection="3d")
-
-ax.plot_surface(X,Y,Z,cmap=cm.coolwarm)
-plt.show()
-
-
+print(func3(5,6,2))
+print(func4(5,6,2))
