a8165e5e048cad0b6568e90fef0af23d84c57ec9
maruyama-k@popular-soft.com-at-723541631475 <maruyama_k@popular-soft.com>
2020-11-12 18:58:43 +0900

    aws

--- Python/Program/sample.py
+++ Python/Program/sample.py
@@ -1,10 +1,25 @@
 import pandas as pd
+from pandas import Series,DataFrame
 
-df =pd.read_csv("http://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data",header=None)
+attr_data1 = {
+    "ID":["100","101","102","103","104","106","108","110","111","113"],
+    "city":["Tokyo","Osaka","Kyoto","Hokkaido","Tokyo","Tokyo","Osaka","Kyoto","Hokkaido","Tokyo"],
+    "birth_year":[1990,1991,1992,1997,1982,1991,1988,1990,1995,1981],
+    "name":["Hiroshi","Akiko","Yuki","Satoru","Steeve","Mituru","Aoi","Tarou","Suguru","Mitsuo"]
+}
 
-df.columns = ["","Alcohol","Malic acid","Ash","Alcalinity of ash","Magnesium","Total phenols", \
-            "Flavanoids","Nonflavanoid phenols","Proanthocyanins","Color intensity","Hue", \
-            "OD280/OD315 of diluted wines","Proline"]
+attr_data1_frame = pd.DataFrame(attr_data1)
 
-print(df)   
+attr_data2 = {
+    "ID":["107","109"],
+    "city":["Sendai","Nagoya"],
+    "birth_year":[1994,1988],
+}
+
+attr_data2_frame = pd.DataFrame(attr_data2)
+
+attr_data1_frame = attr_data1_frame.append(attr_data2_frame).sort_values(by="ID").reset_index(drop=True)
+
+print(attr_data1_frame)
+
 
